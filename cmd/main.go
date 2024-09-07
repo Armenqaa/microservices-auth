97d6cb4a9a668ae799398a33082a9a5aa3910ad4
Armenqaa <thearsenqa@gmail.com>
2024-09-07 15:22:43 +0300

    server with proto

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
-
-	"github.com/fatih/color"
+	desc "github.com/Armenqaa/microservices-auth/pkg/user_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
+	"time"
 )
 
+const (
+	port = 50051
+)
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+// Get ...
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("User id: %d", req.GetId())
+
+	return &desc.GetResponse{
+		Id:        req.GetId(),
+		Name:      "name",
+		Email:     "email",
+		Role:      desc.Role_user,
+		CreatedAt: timestamppb.New(time.Now()),
+		UpdatedAt: timestamppb.New(time.Now()),
+	}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
 }
